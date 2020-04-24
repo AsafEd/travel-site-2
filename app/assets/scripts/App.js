5549441bb78c863dd7165296586f6afe2c9bebca
Asaf <asafroltag@gmail.com>
2020-04-24 12:52:39 +0300

    Completed header desktop and moblile style

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,34 @@
 import '../styles/style.css'
+import MobileMenu from './modules/MobileMenu'
+// import Person from './modules/Person'
+
+
+let mobileMenu = new MobileMenu();
 
 if(module.hot){
     module.hot.accept();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// class Adult extends Person{
+//     payTaxes(){
+//         console.log(this.name + ' now owes zero taxes')
+//     }
+// }
+
+// let p1 = new Person('Johnny');
+//  p1.greet()  ;
+
+//  let p2 = new Adult('Jane');
+//  p2.greet()  ;
+//  p2.payTaxes()  ;
---
+++ app/assets/scripts/modules/Person.js
@@ -0,0 +1,24 @@
+// // function Person(name){
+
+// //     this.name = name;
+// //     // this.greet = 'hello ' + name;
+
+// //    // console.log(this.greet);
+
+// //     this.greet = function(){
+// //         console.log('hello ' + this.name)
+// //     }
+
+// // }
+
+// class Person{
+//     constructor(name){
+//         this.name = name;
+        
+//     }
+//     greet(){
+//         console.log('hello ' + this.name)
+//     }
+// }
+
+// export default Person
